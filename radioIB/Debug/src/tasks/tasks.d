dd2280af8445809f6bedbbe09ffe8b0b5b50456b
unknown <wjv29_admin@6d3k4d1.cusat.cornell.edu>
2014-12-14 15:18:48 -0500

    Added working radioIB code.
    Will add test directory later.

---
+++ radioIB/Debug/src/tasks/tasks.d
@@ -0,0 +1,179 @@
+src/tasks/tasks.d src/tasks/tasks.o: ../src/tasks/tasks.c ../src/asf.h \
+ ../src/asf/common/boards/board.h ../src/asf/xmega/utils/compiler.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/sfr_defs.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iox192a3.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdbool.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stddef.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdlib.h \
+ ../src/asf/xmega/utils/bit_handling/clz_ctz.h \
+ ../src/asf/xmega/utils/preprocessor/preprocessor.h \
+ ../src/asf/xmega/utils/preprocessor/tpaste.h \
+ ../src/asf/xmega/utils/preprocessor/stringz.h \
+ ../src/asf/xmega/utils/preprocessor/mrepeat.h \
+ ../src/asf/xmega/utils/preprocessor/preprocessor.h \
+ ../src/asf/xmega/utils/progmem.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/pgmspace.h \
+ ../src/asf/common/utils/interrupt.h ../src/asf/xmega/utils/parts.h \
+ ../src/asf/common/utils/interrupt/interrupt_avr8.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h \
+ ../src/asf/common/boards/user_board.h \
+ ../src/asf/xmega/drivers/usart/usart.h \
+ ../src/asf/xmega/drivers/pmic/pmic.h ../src/asf/xmega/drivers/cpu/ccp.h \
+ ../src/asf/common/services/serial/serial.h \
+ ../src/asf/common/services/serial/xmega_usart/usart_serial.h \
+ ../src/asf/common/services/clock/sysclk.h ../src/config/conf_clock.h \
+ ../src/asf/common/services/clock/xmega/sysclk.h \
+ ../src/asf/common/services/clock/osc.h \
+ ../src/asf/common/services/clock/xmega/osc.h \
+ ../src/asf/common/services/clock/pll.h \
+ ../src/asf/common/services/clock/xmega/pll.h \
+ ../src/asf/xmega/utils/status_codes.h ../src/tasks/tasks.h \
+ ../src/tasks/../config/conf_board.h ../src/tasks/../memory/memory.h \
+ ../src/tasks/../memory/../vcp/common.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdio.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdarg.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/string.h \
+ ../src/tasks/../memory/LightweightRingBuff.h \
+ c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/atomic.h \
+ ../src/tasks/../memory/dma_driver.h \
+ ../src/tasks/../memory/../tasks/tasks.h \
+ ../src/config/conf_usart_serial.h ../src/config/conf_board.h \
+ ../src/tasks/../memory/../vcp/vcp_library.h \
+ ../src/tasks/../memory/../vcp/common.h \
+ ../src/tasks/../memory/../lithium/lithium.h \
+ ../src/tasks/../memory/../lithium/../vcp/common.h \
+ ../src/tasks/../memory/../lithium/../tasks/tasks.h \
+ ../src/tasks/../memory/../lithium/../vcp/vcp_library.h
+
+../src/asf.h:
+
+../src/asf/common/boards/board.h:
+
+../src/asf/xmega/utils/compiler.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/sfr_defs.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/inttypes.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdint.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdint.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/iox192a3.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/portpins.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdbool.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stddef.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdlib.h:
+
+../src/asf/xmega/utils/bit_handling/clz_ctz.h:
+
+../src/asf/xmega/utils/preprocessor/preprocessor.h:
+
+../src/asf/xmega/utils/preprocessor/tpaste.h:
+
+../src/asf/xmega/utils/preprocessor/stringz.h:
+
+../src/asf/xmega/utils/preprocessor/mrepeat.h:
+
+../src/asf/xmega/utils/preprocessor/preprocessor.h:
+
+../src/asf/xmega/utils/progmem.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/pgmspace.h:
+
+../src/asf/common/utils/interrupt.h:
+
+../src/asf/xmega/utils/parts.h:
+
+../src/asf/common/utils/interrupt/interrupt_avr8.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h:
+
+../src/asf/common/boards/user_board.h:
+
+../src/asf/xmega/drivers/usart/usart.h:
+
+../src/asf/xmega/drivers/pmic/pmic.h:
+
+../src/asf/xmega/drivers/cpu/ccp.h:
+
+../src/asf/common/services/serial/serial.h:
+
+../src/asf/common/services/serial/xmega_usart/usart_serial.h:
+
+../src/asf/common/services/clock/sysclk.h:
+
+../src/config/conf_clock.h:
+
+../src/asf/common/services/clock/xmega/sysclk.h:
+
+../src/asf/common/services/clock/osc.h:
+
+../src/asf/common/services/clock/xmega/osc.h:
+
+../src/asf/common/services/clock/pll.h:
+
+../src/asf/common/services/clock/xmega/pll.h:
+
+../src/asf/xmega/utils/status_codes.h:
+
+../src/tasks/tasks.h:
+
+../src/tasks/../config/conf_board.h:
+
+../src/tasks/../memory/memory.h:
+
+../src/tasks/../memory/../vcp/common.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/stdio.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/include/stdarg.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/string.h:
+
+../src/tasks/../memory/LightweightRingBuff.h:
+
+c:\program\ files\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/atomic.h:
+
+../src/tasks/../memory/dma_driver.h:
+
+../src/tasks/../memory/../tasks/tasks.h:
+
+../src/config/conf_usart_serial.h:
+
+../src/config/conf_board.h:
+
+../src/tasks/../memory/../vcp/vcp_library.h:
+
+../src/tasks/../memory/../vcp/common.h:
+
+../src/tasks/../memory/../lithium/lithium.h:
+
+../src/tasks/../memory/../lithium/../vcp/common.h:
+
+../src/tasks/../memory/../lithium/../tasks/tasks.h:
+
+../src/tasks/../memory/../lithium/../vcp/vcp_library.h:
